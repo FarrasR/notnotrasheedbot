b4d35f8044b488f1c4f52d67441a0362e640540c
FarrasR <farrasrahmatullah@gmail.com>
2021-02-05 13:09:30 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+require('dotenv').config()
+
+const Streamer = require("musix-streamer");
+const ytdl = require("ytdl-core");
+const Speaker = require("speaker");
+var async = require('async');
+const tmi = require('tmi.js');
+
+var songlists=[];
+
+console.log("start playback");
+
+const client = new tmi.Client({
+	options: { debug: true, messagesLogLevel: "info" },
+	connection: {
+		reconnect: true,
+		secure: true
+	},
+	identity: {
+		username: process.env.BOT_USERNAME,
+		password: process.env.BOT_OAUTH
+	},
+	channels: [ process.env.CHANNEL_NAME ]
+});
+client.connect();
+
+
+async function playmusic(lagunya){
+	let promise = new Promise((resolve, reject) => 
+	{
+		var speaker = new Speaker({
+			channels: 2,
+			bitDepth: 16,
+			sampleRate: 44100
+		});
+
+
+		var streamer = new Streamer.default();
+		streamer.stream(ytdl(lagunya, { filter: format => format.container === 'mp4' })).pipe(speaker);
+		speaker.on('flush', function()
+		{
+			// console.log("kekw");
+			resolve(true);
+		});
+	}
+	).catch(e => { console.log(e) });
+
+	let result = await promise;
+	return result;
+
+};
+
+function waitqueue(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+async function playqueue(){
+	// await playmusic("https://www.youtube.com/watch?v=__1SjDrSMik");
+	while(true)
+	{
+		await waitqueue(1000);
+		console.log(songlists.length)
+		if(songlists.length>0)
+		{
+			var cursong = songlists.shift()
+			console.log(cursong)
+			// if (ytdl.validateURL(cursong))
+			// 		{
+			// 			console.log("valid")
+			// 			console.log(ytdl.getVideoID(cursong))
+			// 		}
+			// 		else
+			// 		{
+			// 			console.log("not valid")		
+			// 		}
+			
+			await playmusic(cursong);
+			
+			// console.log(songlists.length) 
+		}
+		else
+			console.log("queue empty")
+	}
+}
+
+songlists.push("https://www.youtube.com/watch?v=BHsLOoXfqtY")
+playqueue()
+
+
+client.on('message', (channel, tags, message, self) => {
+	console.log(`${tags['display-name']}: ${message}`);
+	if(message.toLowerCase() === "hello")
+	{
+		client.say(channel, `@${tags.username}, heya!`);
+	} 
+	else 
+	{
+		console.log("SBUAIDHBASUIDBSUAI");
+		if(ytdl.getURLVideoID(message))
+		{
+			console.log("VideoID valid");
+		}
+		else
+		{
+			console.log("VideoID not valid");		
+		}
+		if (ytdl.validateURL(message))
+		{
+			console.log("valid");
+			console.log(ytdl.getVideoID(message));
+			songlists.push(message);	
+		}
+		else
+		{
+			console.log("not valid");		
+		}
+
+  	}
+});
